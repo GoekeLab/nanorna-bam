26aab2598de07d00b5ac8404805cc20acc1e62e2
lwratten <z5164169@unsw.edu.au>
2019-12-30 15:41:36 +0800

    add in Workflow.onComplete

--- bin/scrape_software_versions.py
+++ bin/scrape_software_versions.py
@@ -5,16 +5,16 @@ import re
 
 # TODO nf-core: Add additional regexes for new tools in process get_software_versions
 regexes = {
-    'nf-core/nanornabam': ['v_pipeline.txt', r"(\S+)"],
-    'Nextflow': ['v_nextflow.txt', r"(\S+)"],
-    'FastQC': ['v_fastqc.txt', r"FastQC v(\S+)"],
-    'MultiQC': ['v_multiqc.txt', r"multiqc, version (\S+)"],
+    'nf-core/nanornabam': ['pipeline.version', r"(\S+)"],
+    'Nextflow': ['nextflow.version', r"(\S+)"],
+    'StringTie2': ['stringtie.version', r"(\S+)"],
+    'FeatureCounts': ['featureCounts.version', r"featureCounts v(\S+)"],
 }
 results = OrderedDict()
 results['nf-core/nanornabam'] = '<span style="color:#999999;\">N/A</span>'
 results['Nextflow'] = '<span style="color:#999999;\">N/A</span>'
-results['FastQC'] = '<span style="color:#999999;\">N/A</span>'
-results['MultiQC'] = '<span style="color:#999999;\">N/A</span>'
+results['StringTie2'] = '<span style="color:#999999;\">N/A</span>'
+results['FeatureCounts'] = '<span style="color:#999999;\">N/A</span>'
 
 # Search each file using its regex
 for k, v in regexes.items():
