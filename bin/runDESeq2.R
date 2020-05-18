cf1786a26bb89bfa8d7a45c7d5809fa80f897cb0
cying111 <chen_ying@gis.a-star.edu.sg>
2020-05-18 10:22:42 +0000

    Small changes to replace T with TRUE

--- bin/runDESeq2.R
+++ bin/runDESeq2.R
@@ -18,14 +18,14 @@ if (length(args) < 2) {
 }
 #DeSeq2
 path<-args[1]
-count_files<- grep(list.files(path), pattern='tx_', inv=T, value=T)
+count_files<- grep(list.files(path), pattern='tx_', inv= TRUE, value= TRUE)
 #create a dataframe for all samples 
 fullpath<-paste(path,count_files[1],sep='/') 
-df <- data.frame(read.table(fullpath,sep="\t",header=T)[,c(1,7)])
+df <- data.frame(read.table(fullpath,sep="\t",header= TRUE)[,c(1,7)])
 for(i in 2:length(count_files)){
   fullpath<-paste(path,count_files[i],sep='/') 
-  samp_df <- read.table(fullpath,sep="\t",header=T)[,c(1,7)]
-  df<- merge(df,samp_df,by="Geneid",all=TRUE)
+  samp_df <- read.table(fullpath,sep="\t",header= TRUE)[,c(1,7)]
+  df<- merge(df,samp_df,by="Geneid",all= TRUE)
 }
 countTab <- df[,-1]
 rownames(countTab) <- df[,1]
