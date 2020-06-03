43ddea4e6d94b44b978e5c9bcc732b9f759d7c9c
Yuk Kei Wan <41866052+yuukiiwa@users.noreply.github.com>
2020-06-03 17:26:11 +0800

    Added in the option of using Bambu count files

--- bin/runDESeq2.R
+++ bin/runDESeq2.R
@@ -10,34 +10,43 @@ if (!require("DESeq2")){
 
 
 args = commandArgs(trailingOnly=TRUE)
-if (length(args) < 2) {
+if (length(args) < 3) {
   stop("Please input the directory with the featureCounts results and the sample information file", call.=FALSE)
-} else if (length(args)==2) {
+} else if (length(args)==3) {
   # default output file
-  args[3] = "out.txt"
+  args[4] = "DESeq2out.txt"
 }
 #DeSeq2
-path<-args[1]
-count_files<- grep(list.files(path), pattern='tx_', inv=T, value=T)
-#create a dataframe for all samples 
-fullpath<-paste(path,count_files[1],sep='/')
-count.matrix <- data.frame(read.table(fullpath,sep="\t",header=T)[,c(1,7,8)])
-for(i in 2:length(count_files)){
-  fullpath<-paste(path,count_files[i],sep='/')
-  samp_df <- read.table(fullpath,sep="\t",header=T)[,c(1,8)]
-  count.matrix<- merge(count.matrix,samp_df,by="Geneid",all=TRUE)
+transcriptquant <- args[1]
+path<-args[2]
+count_files<- grep(list.files(path), pattern='tx_', inv=TRUE, value=TRUE)
+#create a dataframe for all samples
+if (identical(transcriptquant,"stringtie") == TRUE){
+  count_files<- grep(list.files(path), pattern='tx_', inv=TRUE, value=TRUE)
+  fullpath<-paste(path,count_files[1],sep='/')
+  count.matrix <- data.frame(read.table(fullpath,sep="\t",header=TRUE)[,c(1,7,8)])
+  for(i in 2:length(count_files)){
+    fullpath<-paste(path,count_files[i],sep='/')
+    samp_df <- read.table(fullpath,sep="\t",header=T)[,c(1,8)]
+    count.matrix<- merge(count.matrix,samp_df,by="Geneid",all=TRUE)
+  }
+  df <- count.matrix[,-1]
+  rownames(df) <- count.matrix[,1]
+  df <- aggregate(. ~ gene_id, data=df, FUN=sum)
+  countTab <- df[,-1]
+  rownames(countTab) <- df[,1]
+}else if (identical(transcriptquant,"bambu") == TRUE){
+  countTab <- data.frame(read.table(path,sep="\t",header=TRUE))
 }
-df <- count.matrix[,-1]
-rownames(df) <- count.matrix[,1]
-df <- aggregate(. ~ gene_id, data=df, FUN=sum)
-countTab <- df[,-1]
-rownames(countTab) <- df[,1]
-sampInfo<-read.csv(args[2],row.names=1)
-all(rownames(sampInfo) %in% colnames(countTab))
-all(rownames(sampInfo) == colnames(countTab))
+
+#sampInfo <- read.csv("~/Downloads/nanorna-bam-master/two_conditions.csv",row.names = 1)
+sampInfo<-read.csv(args[3],row.names=1)
+countTab <- data.frame(read.table("~/Downloads/nanorna-bam-master/results/Bambu/counts_gene.txt"))
+#all(rownames(sampInfo) %in% colnames(countTab))
+#all(rownames(sampInfo) == colnames(countTab))
 dds <- DESeqDataSetFromMatrix(countData = countTab,colData = sampInfo,design = ~ condition)
 dds <- DESeq(dds)
 res <- results(dds)
 #register(MulticoreParam(6))
 resOrdered <- res[order(res$pvalue),]
-write.csv(as.data.frame(resOrdered), file=args[3])
+write.csv(as.data.frame(resOrdered), file=args[4])
--- bin/runDEXseq.R
+++ bin/runDEXseq.R
@@ -14,29 +14,32 @@ if (!require("stageR")){
 }
 
 args = commandArgs(trailingOnly=TRUE)
-if (length(args) < 2) {
+if (length(args) < 3) {
   stop("Please input the directory with the transcript level featureCounts results and the sample information file", call.=FALSE)
-} else if (length(args)==2) {
-  # default output file
-  args[3] = "DEXseq_out.txt"
-}
+} 
 ######### import data ###########
 ##Import featureCounts data
 #path <- "~/Downloads/nanorna-bam-master/mod/featureCounts_transcript/"
-path<-args[1]
-count_files<- grep(list.files(path), pattern='tx_', inv=T, value=T)
-#create a dataframe for all samples 
-fullpath<-paste(path,count_files[1],sep='/') 
-count.matrix <- data.frame(read.table(fullpath,sep="\t",header=T)[,c(1,7,8)])
-for(i in 2:length(count_files)){
-  fullpath<-paste(path,count_files[i],sep='/') 
-  samp_df <- read.table(fullpath,sep="\t",header=T)[,c(1,8)]
-  count.matrix<- merge(count.matrix,samp_df,by="Geneid",all=TRUE)
+transcriptquant <- args[1]
+path<-args[2]
+if (identical(transcriptquant,"stringtie") == TRUE){
+  count_files<- grep(list.files(path), pattern='tx_', inv=T, value=T)
+  #create a dataframe for all samples 
+  fullpath<-paste(path,count_files[1],sep='/') 
+  count.matrix <- data.frame(read.table(fullpath,sep="\t",header=T)[,c(1,7,8)])
+  for(i in 2:length(count_files)){
+    fullpath<-paste(path,count_files[i],sep='/') 
+    samp_df <- read.table(fullpath,sep="\t",header=T)[,c(1,8)]
+    count.matrix<- merge(count.matrix,samp_df,by="Geneid",all=TRUE)
+  }
+}else if (identical(transcriptquant,"bambu") == TRUE){
+  count.matrix <- data.frame(read.table(path,sep="\t",header=T))
 }
 colnames(count.matrix)[1] <- "feature_id"
+colnames(count.matrix)[2] <- "gene_id"
 #sample information
 #sampleinfo<-read.table("~/Downloads/nanorna-bam-master/samples_conditions.csv",sep=",",header=T)
-sampleinfo<-read.table(args[2],sep=",",header=T)
+sampleinfo<-read.table(args[3],sep=",",header=T)
 colnames(sampleinfo)[1] <- "sample_id"
 condition_names <- sampleinfo[!duplicated(sampleinfo$condition),]$condition
 #condition_names <- c(levels(sampleinfo$condition))
@@ -74,7 +77,7 @@ dxd <- estimateSizeFactors(dxd)
 print('Size factor estimated')
 dxd <- estimateDispersions(dxd, formula = formulaFullModel)
 print('Dispersion estimated')
-dxd <- testForDEU(dxd, reducedModel = formulaReducedModel, fullModel = formulaFullModel)
+dxd <- testForDEU(dxd, fullModel = formulaFullModel)
 print('DEU tested')
 dxd <- estimateExonFoldChanges(dxd)
 print('Exon fold changes estimated')
