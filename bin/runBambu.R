ee6801e785b6f12453ef083bf920beb72b634da2
Yuk Kei Wan <41866052+yuukiiwa@users.noreply.github.com>
2020-06-06 12:07:45 +0800

    add in the option of using fasta as genomeSequence

--- bin/runBambu.R
+++ bin/runBambu.R
@@ -1,12 +1,7 @@
 #!/usr/bin/env Rscript
 
-if (!requireNamespace("BiocManager", quietly = TRUE)){
-    install.packages("BiocManager", repos='http://cran.us.r-project.org')
- }
-if (!require("BSgenome.Hsapiens.NCBI.GRCh38")){
-    BiocManager::install("BSgenome.Hsapiens.NCBI.GRCh38",update = FALSE, ask= FALSE)
-    library(BSgenome.Hsapiens.NCBI.GRCh38)
-}
+##install bambu if bambu is not installed
+##load bambu
 if (!requireNamespace("devtools", quietly = TRUE)){
     install.packages("devtools", repos='http://cran.us.r-project.org')
 }
@@ -16,20 +11,28 @@ if (!require("bambu")){
 }
 
 args = commandArgs(trailingOnly=TRUE)
-if (length(args) < 2) {
+if (length(args) < 3) {
   stop("Please input the fullpath for the present directory and the sample sheet.", call.=FALSE)
-} else if (length(args)==2) {
-  args[2] = "Bambu_outputs"
+} else if (length(args)==3) {
+  if (!requireNamespace("BiocManager", quietly = TRUE)){
+    install.packages("BiocManager", repos='http://cran.us.r-project.org')
+  }
+  if (!require("BSgenome.Hsapiens.NCBI.GRCh38")){
+    BiocManager::install("BSgenome.Hsapiens.NCBI.GRCh38",update = FALSE, ask= FALSE)
+    library(BSgenome.Hsapiens.NCBI.GRCh38)
+  }
+  genomeseq <- BSgenome.Hsapiens.NCBI.GRCh38  #use BSgenome if fasta file is not provided
 }
 pwd <- args[1]
 sampInfo <- read.table(args[2], header = TRUE, sep=",")
 annotation_list <- unique(sampInfo$annotation)
 output_tag <- args[3]
+genomeseq <- args[4]
 for(i in 1:length(annotation_list)){
     readlist <- sampInfo[sampInfo$annotation %in% annotation_list[i]]$bam
     annot_gtf <- paste(pwd,annotation_list[i],sep="/")
     readlist <- paste(pwd,readlist,sep="/")
     grlist <- prepareAnnotationsFromGTF(annot_gtf)
-    se <- bambu(reads = readlist, annotations=grlist,genomeSequence = "BSgenome.Hsapiens.NCBI.GRCh38")
+    se <- bambu(reads = readlist, annotations=grlist,genomeSequence = genomeseq)
     writeBambuOutput(se,output_tag)
 }
