38a2481b03f871efcd0a16f37e8ed04e7f96d7fb
cying111 <chen_ying@gis.a-star.edu.sg>
2020-06-23 07:16:10 +0000

    Simplied code a bit

--- bin/runDEXseq.R
+++ bin/runDEXseq.R
@@ -26,7 +26,7 @@ if (length(args) < 3) {
 #path <- "~/Downloads/nanorna-bam-master/mod/featureCounts_transcript/"
 transcriptquant <- args[1]
 path<-args[2]
-if (identical(transcriptquant,"stringtie") == TRUE){
+if (transcriptquant == "stringtie"){
   count_files<- grep(list.files(path), pattern='tx_', inv=T, value=T)
   #create a dataframe for all samples 
   fullpath<-paste(path,count_files[1],sep='/') 
@@ -36,8 +36,10 @@ if (identical(transcriptquant,"stringtie") == TRUE){
     samp_df <- read.table(fullpath,sep="\t",header=T)[,c(1,8)]
     count.matrix<- merge(count.matrix,samp_df,by="Geneid",all=TRUE)
   }
-}else if (identical(transcriptquant,"bambu") == TRUE){
-  count.matrix <- data.frame(read.table(path,sep="\t",header=T))
+}
+
+if (transcriptquant == "bambu"){
+  count.matrix <- data.frame(read.table(dir(path, pattern = "counts_transcript", full.names = TRUE),sep="\t",header=T))
 }
 colnames(count.matrix)[1] <- "feature_id"
 colnames(count.matrix)[2] <- "gene_id"
